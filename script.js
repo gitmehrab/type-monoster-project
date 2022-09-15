ad826394492e2d688a6244c20b3b811cf58c6f15
gitmehrab <hasanmehrab224@gmail.com>
2022-09-15 15:31:37 +0600

    parse time count

--- script.js
+++ script.js
@@ -72,6 +72,7 @@ const gameOver = () => {
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
   const timeTaken = (finishTime - startTime) / 1000;
+  const timeTakenNumber = parseInt(timeTaken);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -84,7 +85,7 @@ const gameOver = () => {
   // show result
   resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+    <p>You took: <span class="bold">${timeTakenNumber}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
